1b0eeabc01227f2f573af723578653bd83f632ad
BitPulse01 <bitpulse0101@gmail.com>
2024-08-14 11:25:29 +0330

    SCRAPING - getting the Authors

--- scraping.py
+++ scraping.py
@@ -12,7 +12,14 @@ class scraper:
         self.DRIVER: WebDriver = WB.Chrome()
         self.DRIVER.get(self.URL)
         self.DRIVER.maximize_window()
-
+        
+        time.sleep(5)
+        
+        self.AUTHOR_CONTAINER = self.DRIVER.find_elements(By.XPATH, '//*[@class="flex justify-between text-12 min-h-[32px]  mb-2xs mt-[-4px]"]/span/span/div')
+        
+        for index, self.AUTHOR_PARENT in enumerate(self.AUTHOR_CONTAINER):
+            print(self.AUTHOR_PARENT.text)
+        
         # time.sleep(555)
 
 
