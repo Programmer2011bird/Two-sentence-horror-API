ac77a985483c66d5dd89f18db8c8f442b2bc8877
BitPulse01 <bitpulse0101@gmail.com>
2024-08-15 15:30:45 +0330

    minor changes and type annotations

--- scraping.py
+++ scraping.py
@@ -1,8 +1,5 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import selenium.webdriver as WB
 from typing import Any
@@ -10,45 +7,45 @@ import time
 
 
 class scraper:
-    def __init__(self, content_date: str = "hot", tops_date: str = "today") -> None:
-        match content_date.lower():
+    def __init__(self, content_type: str = "hot", tops_date: str = "today") -> None:
+        match content_type.lower():
             case "new":
                 self.URL = "https://www.reddit.com/r/TwoSentenceHorror/new/"
             
             case "top":
                 match tops_date.lower():
                     case "today":
-                        self.URL = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=day"
+                        self.URL: str= "https://www.reddit.com/r/TwoSentenceHorror/top/?t=day"
                     
                     case "this week":
-                        self.URL = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=week"
+                        self.URL: str = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=week"
                     
                     case "this month":
-                        self.URL = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=month"
+                        self.URL: str = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=month"
                     
                     case "this year":
-                        self.URL = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=year"
+                        self.URL: str = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=year"
                     
                     case "all time":
-                        self.URL = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=all"
+                        self.URL: str = "https://www.reddit.com/r/TwoSentenceHorror/top/?t=all"
             
             case "hot":
                 self.URL: str = "https://www.reddit.com/r/TwoSentenceHorror/"
         
         time.sleep(5)
+
         self.DRIVER: WebDriver = WB.Chrome()
         self.DRIVER.get(self.URL)
         self.DRIVER.maximize_window()
         
         time.sleep(5)
         
-    def get_info(self):
+    def get_info(self) -> list[dict[str, str]]:
         self.POSTS_LIST: list[dict[str, str]] = []
 
         while True:
             posts: list[WebElement] = self.DRIVER.find_elements(By.TAG_NAME, "shreddit-post")
             
-
             self.LAST_HEIGHT: Any = self.DRIVER.execute_script("return document.body.scrollHeight;")
             self.DRIVER.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             
@@ -87,8 +84,3 @@ class scraper:
                     }
 
                     self.POSTS_LIST.append(self.POST_INFO)
-
-
-if __name__ == "__main__":
-    SCRAPER: scraper = scraper(content_date="top")
-    print(SCRAPER.get_info())
