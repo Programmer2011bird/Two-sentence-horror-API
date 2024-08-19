97f8b4ffeadbad09b300eabf35bf1a1c73814545
BitPulse01 <bitpulse0101@gmail.com>
2024-08-19 11:42:13 +0330

    Adding functions as parameters to add extra functionallity while looping

--- Telegram_Bot/API_handler.py
+++ Telegram_Bot/API_handler.py
@@ -4,17 +4,18 @@ import requests
 
 class API_controller:
     def __init__(self) -> None:
-        self.URL = "http://127.0.0.1:8000/?content_type=new"
+        self.URL: str = "http://127.0.0.1:8000/?content_type=new"
 
-        self.RESPONSE = requests.get(self.URL)
+        self.RESPONSE: requests.Response = requests.get(self.URL)
         self.RESPONSE_JSON: list[dict[str, str]] = self.RESPONSE.json()
 
+    def get_info(self, func) -> None:
         for index, self.POST in enumerate(self.RESPONSE_JSON):
-            self.AUTHOR = self.POST.get("Author", None)
-            self.FIRST_SENTENCE = self.POST.get("First_Sentence", None)
-            self.SECOND_SENTENCE = self.POST.get("Second_Sentence", None)
-            self.NUMBER_OF_UPVOTES = self.POST.get("Number_of_Upvotes", None)
-            self.NUMBER_OF_COMMENTS = self.POST.get("Number_of_Comments", None)
+            self.AUTHOR: str | None = self.POST.get("Author", None)
+            self.FIRST_SENTENCE: str | None = self.POST.get("First_Sentence", None)
+            self.SECOND_SENTENCE: str | None = self.POST.get("Second_Sentence", None)
+            self.NUMBER_OF_UPVOTES: str | None = self.POST.get("Number_of_Upvotes", None)
+            self.NUMBER_OF_COMMENTS: str | None = self.POST.get("Number_of_Comments", None)
 
             print(f'{Fore.MAGENTA} ---------------------------{index}---------------------------')
             print(f"{Fore.LIGHTGREEN_EX}AUTHOR : {Fore.CYAN}{self.AUTHOR}")
@@ -23,6 +24,4 @@ class API_controller:
             print(f"{Fore.LIGHTGREEN_EX}NUMBER OF UPVOTES : {Fore.CYAN}{self.NUMBER_OF_UPVOTES}")
             print(f"{Fore.LIGHTGREEN_EX}NUMBER OF COMMENTS : {Fore.CYAN}{self.NUMBER_OF_COMMENTS}")
 
-
-if __name__ == "__main__":
-    API = API_controller()
+            func()
